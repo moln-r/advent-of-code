750693cc27257105d6968bd6b8116053d977abf7
Máté Molnár <matemolnar@protonmail.com>
2025-08-21 22:09:57 +0200

    moving first day second solution too

--- 2023/days/day01/day01.go
+++ 2023/days/day01/day01.go
@@ -22,17 +22,20 @@ func (solver) Solve(input string) (string, string, error) {
 func solvePart1(lines []string) string {
 	sum := 0
 	for _, line := range lines {
-		sum = sum + numFromLine(line)
+		sum = sum + numFromLine(line, false)
 	}
 	return strconv.Itoa(sum)
 }
 
 func solvePart2(lines []string) string {
-	fmt.Println(lines[0])
-	return "TODO"
+	sum := 0
+	for _, line := range lines {
+		sum = sum + numFromLine(line, true)
+	}
+	return strconv.Itoa(sum)
 }
 
-func numFromLine(line string) int {
+func numFromLine(line string, writtenNums bool) int {
 	if line == "" {
 		return 0
 	}
@@ -40,13 +43,22 @@ func numFromLine(line string) int {
 	first := 0
 	last := 0
 
-	for _, r := range line {
+	for i, r := range line {
 		num, err := charAsNum(r)
 		if err == nil {
 			if first == 0 {
 				first = num
 			}
 			last = num
+		}
+		if writtenNums {
+			num, err = numFromThisChar([]rune(line)[i:])
+			if err == nil {
+				if first == 0 {
+					first = num
+				}
+				last = num
+			}
 		}
 	}
 
@@ -59,4 +71,38 @@ func charAsNum(c rune) (int, error) {
 		return int(c - '0'), nil
 	}
 	return 0, fmt.Errorf("not numeric")
+}
+
+var numbers = [][]rune{
+	{'o', 'n', 'e'},
+	{'t', 'w', 'o'},
+	{'t', 'h', 'r', 'e', 'e'},
+	{'f', 'o', 'u', 'r'},
+	{'f', 'i', 'v', 'e'},
+	{'s', 'i', 'x'},
+	{'s', 'e', 'v', 'e', 'n'},
+	{'e', 'i', 'g', 'h', 't'},
+	{'n', 'i', 'n', 'e'},
+}
+
+func numFromThisChar(substring []rune) (int, error) {
+	for numberIndex, number := range numbers {
+		hit := true
+		if len(substring) < len(number) {
+			// number cannot fit, we skip it
+			continue
+		}
+
+		for i := range number {
+			if substring[i] == number[i] {
+				continue
+			} else {
+				hit = false
+			}
+		}
+		if hit {
+			return numberIndex + 1, nil
+		}
+	}
+	return 0, fmt.Errorf("no number starts from this character")
 }
