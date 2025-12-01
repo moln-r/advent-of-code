c7688e49f31d3a038a71ff2bd8307525355b88bd
Máté Molnár <matemolnar@protonmail.com>
2025-12-01 15:50:37 +0100

    i'm not proud of this one

--- 2025/app/src/main/java/org/example/Day01.java
+++ 2025/app/src/main/java/org/example/Day01.java
@@ -1,52 +1,34 @@
 package org.example;
 
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.BiFunction;
-
 public class Day01 extends Day {
 
-  private final AtomicInteger atZero;
-  private final AtomicInteger clicks;
+  private final CounterForIdiots counter;
 
   public Day01(boolean debug) {
     super("01", debug);
-    this.atZero = new AtomicInteger(0);
-    this.clicks = new AtomicInteger(0);
-
-    processInput();
+    this.counter = new CounterForIdiots(50);
+    processLikeAnIdiot();
   }
 
-  private void processInput() {
-    BiFunction<Integer, String, Integer> moveDial =
-        (currentPosition, input) -> {
-          var direction = Direction.get(input);
-          int rotations = Integer.parseInt(input.substring(1));
-
-          clicks.getAndAdd(direction.getClicks(currentPosition, rotations));
-
-          int relevantRotations = rotations % 100;
-
-          int newPosition = direction.nextPosition(currentPosition, relevantRotations);
-
-          if (newPosition == 0) {
-            atZero.getAndIncrement();
-          }
-          return newPosition;
-        };
-    int current = 50;
+  private void processLikeAnIdiot() {
     for (String line : input) {
-      current = moveDial.apply(current, line);
+      var direction = Direction.get(line);
+      int rotations = Integer.parseInt(line.substring(1));
+      switch (direction) {
+        case LEFT -> counter.decrement(rotations);
+        case RIGHT -> counter.increment(rotations);
+      }
     }
   }
 
   @Override
   public void part1() {
-    log("day 1 part 1: " + atZero.get());
+    log("day 1 part 1: " + counter.atZero);
   }
 
   @Override
   public void part2() {
-    log("day 1 part 2: " + clicks.get());
+    log("day 1 part 2: " + counter.clicks);
   }
 
   private enum Direction {
@@ -61,35 +43,50 @@ public class Day01 extends Day {
       }
       throw new RuntimeException("invalid direction");
     }
+  }
 
-    private int nextPosition(int position, int diff) {
-      var newPosition =
-          switch (this) {
-            case LEFT -> position - diff;
-            case RIGHT -> position + diff;
-          };
-      if (newPosition < 0) {
-        return newPosition + 100;
-      } else if (newPosition >= 100) {
-        return newPosition - 100;
-      } else {
-        return newPosition;
+  private static class CounterForIdiots {
+    private int value;
+    private int atZero;
+    private int clicks;
+
+    private CounterForIdiots(int value) {
+      this.value = value;
+      this.atZero = 0;
+      this.clicks = 0;
+    }
+
+    private void increment(int by) {
+      for (int i = 0; i < by; i++) {
+        value += 1;
+
+        if (value == 100) {
+          this.value = 0;
+        }
+
+        if (value == 0) {
+          this.clicks++;
+        }
+      }
+      if (value == 0) {
+        this.atZero++;
       }
     }
 
-    private int getClicks(int start, int rotation) {
-      if (this.equals(LEFT)) {
-        var end = start - rotation;
-        if (end > 0) {
-          return 0;
+    private void decrement(int by) {
+      for (int i = 0; i < by; i++) {
+        value -= 1;
+
+        if (value == -1) {
+          this.value = 99;
         }
-        return end / -100 + 1;
-      } else {
-        var end = start + rotation;
-        if (end < 100) {
-          return 0;
+
+        if (value == 0) {
+          this.clicks++;
         }
-        return end / 100;
+      }
+      if (value == 0) {
+        this.atZero++;
       }
     }
   }
