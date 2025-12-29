741dfcaf16a9aa320bb1c5334477c5b08a723b9d
Máté Molnár <matemolnar@protonmail.com>
2025-12-29 18:26:43 +0100

    day 9 part 2 - not working

--- 2025/app/src/main/java/org/example/Day09.java
+++ 2025/app/src/main/java/org/example/Day09.java
@@ -2,50 +2,45 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TreeSet;
 
 public class Day09 extends Day {
 
-  private final List<Rectangle> rectangles;
+  final Layout layout;
+
+  //  + - - > x
+  //  |
+  //  |
+  //  V y
 
   public Day09(boolean debug) {
     super("09", debug);
-
-    this.rectangles = new ArrayList<>();
-    for (int i = 0; i < input.size(); i++) {
-      var first = new Point(input.get(i));
-      for (int j = i + 1; j < input.size(); j++) {
-        var second = new Point(input.get(j));
-        rectangles.add(new Rectangle(first, second));
-      }
-    }
+    this.layout = new Layout(input);
   }
 
   @Override
   public long part1() {
-    var solution = findLargestArea();
+    var solution = layout.findLargestArea();
     System.out.println("day 9 part 1: " + solution);
     return solution;
   }
 
-  protected long findLargestArea() {
-    return rectangles.stream().map(Rectangle::area).max(Long::compareTo).orElse(0L);
-  }
-
   @Override
   public long part2() {
-    var solution = 0L;
+    var solution = layout.findLargestInArea();
     System.out.println("day 9 part 2: " + solution);
+    // 4605771104 too high
     return solution;
   }
 
-  private record Point(int x, int y) {
+  record Point(int x, int y) {
     private Point(String s) {
       var split = s.split(",");
       this(Integer.parseInt(split[0]), Integer.parseInt(split[1]));
     }
   }
 
-  private record Rectangle(Point first, Point second, Point p1, Point p2, long area) {
+  record Rectangle(Point first, Point second, Point p1, Point p2, long area) {
     private Rectangle(Point first, Point second) {
       this(
           first,
@@ -53,6 +48,105 @@ public class Day09 extends Day {
           new Point(first.x, second.y),
           new Point(second.x, first.y),
           (Math.abs(first.x - second.x) + 1L) * (Math.abs(first.y - second.y) + 1L));
+    }
+  }
+
+  record Line(
+      Point a,
+      Point b,
+      boolean horizontal,
+      boolean vertical,
+      int lineIndex,
+      int start,
+      int finish) {
+
+    private Line(Point a, Point b) {
+      boolean vertical = a.x == b.x;
+      boolean horizontal = a.y == b.y;
+      var lineIndex = horizontal ? a.y : a.x;
+      var start = vertical ? Math.min(a.y, b.y) : Math.min(a.x, b.x);
+      var end = vertical ? Math.max(a.y, b.y) : Math.max(a.x, b.x);
+      this(a, b, horizontal, vertical, lineIndex, start, end);
+    }
+  }
+
+  record Layout(
+      List<Line> horizontals, List<Line> verticals, TreeSet<Rectangle> orderedRectangles) {
+    Layout(List<String> input) {
+      var orderedRectangles =
+          new TreeSet<Rectangle>(
+              (o1, o2) -> {
+                long value = o2.area - o1.area;
+                if (value == 0) return 0;
+                if (value > 0) return 1;
+                return -1;
+              });
+      var lines = new ArrayList<Line>();
+      for (int i = 0; i < input.size(); i++) {
+        var first = new Point(input.get(i));
+        for (int j = i + 1; j < input.size(); j++) {
+          var second = new Point(input.get(j));
+          var rectangle = new Rectangle(first, second);
+          orderedRectangles.add(rectangle);
+          lines.add(new Line(first, second));
+        }
+      }
+      this(
+          lines.stream().filter(Line::horizontal).toList(),
+          lines.stream().filter(Line::vertical).toList(),
+          orderedRectangles);
+    }
+
+    long findLargestArea() {
+      return orderedRectangles.getFirst().area;
+    }
+
+    long findLargestInArea() {
+      for (Rectangle rectangle : orderedRectangles) {
+        if (isIn(rectangle)) {
+          return rectangle.area;
+        }
+      }
+      return 0L;
+    }
+
+    boolean isIn(Rectangle r) {
+      return isIn(r.p1) && isIn(r.p2);
+    }
+
+    boolean isIn(Point p) {
+      return isOnLine(p) || (isInVertically(p) && isInHorizontally(p));
+    }
+
+    boolean isOnLine(Point p) {
+      return horizontals.stream()
+              .filter(line -> line.start <= p.x && p.x <= line.finish)
+              .anyMatch(line -> p.y == line.lineIndex)
+          || verticals.stream()
+              .filter(line -> line.start <= p.y && p.y <= line.finish)
+              .anyMatch(line -> p.x == line.lineIndex);
+    }
+
+    boolean isInHorizontally(Point p) {
+      var relevantLines =
+          horizontals.stream().filter(line -> line.start <= p.x && p.x <= line.finish).toList();
+
+      var linesBefore = relevantLines.stream().filter(line -> line.lineIndex < p.y).count();
+      var linesAfter = relevantLines.stream().filter(line -> line.lineIndex > p.y).count();
+
+      // we have to pass lines odd number of times to be inside
+      return linesBefore % 2 != 0 || linesAfter % 2 != 0;
+    }
+
+    boolean isInVertically(Point p) {
+      var relevantLines =
+          verticals.stream().filter(line -> line.start <= p.y && p.y <= line.finish).toList();
+
+      var linesBefore = relevantLines.stream().filter(line -> line.lineIndex < p.x).count();
+      var linesAfter = relevantLines.stream().filter(line -> line.lineIndex > p.x).count();
+
+      // we have to pass lines odd number of times to be inside
+      return linesBefore % 2 != 0 || linesAfter % 2 != 0;
     }
   }
 }
--- 2025/app/src/test/java/org/example/Day09Test.java
+++ 2025/app/src/test/java/org/example/Day09Test.java
@@ -2,24 +2,127 @@ package org.example;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.example.Day09.Layout;
+import org.example.Day09.Point;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class Day09Test {
 
   private Day09 day;
+  private Layout layout;
 
   @BeforeEach
   void setUp() {
     day = new Day09(true);
+    layout = day.layout;
   }
 
   @Test
   void testPart1() {
-    var area = day.findLargestArea();
+    var area = layout.findLargestArea();
     assertEquals(50, area);
   }
 
   @Test
-  void testPart2() {}
+  void testPart2() {
+    var area = layout.findLargestInArea();
+    assertEquals(24, area);
+  }
+
+  @Test
+  void testInHorizontally() {
+    assertTrue(layout.isInHorizontally(new Point(5, 5)));
+    assertFalse(layout.isInHorizontally(new Point(0, 5)));
+  }
+
+  @Test
+  void testInVertically() {
+    assertTrue(layout.isInVertically(new Point(1, 8)));
+    assertFalse(layout.isInVertically(new Point(1, 5)));
+  }
+
+  @Test
+  void testPointIn() {
+    var all = new ArrayList<Point>();
+    for (int i = 1; i < 12; i++) {
+      for (int j = 1; j < 12; j++) {
+        all.add(new Point(i, j));
+      }
+    }
+    var inPoints =
+        List.of(
+            new Point(2, 3),
+            new Point(2, 4),
+            new Point(2, 5),
+            new Point(3, 3),
+            new Point(3, 4),
+            new Point(3, 5),
+            new Point(4, 3),
+            new Point(4, 4),
+            new Point(4, 5),
+            new Point(5, 3),
+            new Point(5, 4),
+            new Point(5, 5),
+            new Point(6, 3),
+            new Point(6, 4),
+            new Point(6, 5),
+            new Point(7, 1),
+            new Point(7, 2),
+            new Point(7, 3),
+            new Point(7, 4),
+            new Point(7, 5),
+            new Point(8, 1),
+            new Point(8, 2),
+            new Point(8, 3),
+            new Point(8, 4),
+            new Point(8, 5),
+            new Point(9, 1),
+            new Point(9, 2),
+            new Point(9, 3),
+            new Point(9, 4),
+            new Point(9, 5),
+            new Point(9, 6),
+            new Point(9, 7),
+            new Point(10, 1),
+            new Point(10, 2),
+            new Point(10, 3),
+            new Point(10, 4),
+            new Point(10, 5),
+            new Point(10, 6),
+            new Point(10, 7),
+            new Point(11, 1),
+            new Point(11, 2),
+            new Point(11, 3),
+            new Point(11, 4),
+            new Point(11, 5),
+            new Point(11, 6),
+            new Point(11, 7));
+
+    all.removeAll(inPoints);
+
+    for (Point p : inPoints) {
+      assertTrue(layout.isIn(p), "not in: " + p);
+    }
+    for (Point p : all) {
+      assertFalse(layout.isIn(p), "in: " + p);
+    }
+
+    assertTrue(inPoints.stream().allMatch(layout::isIn));
+  }
 }
+
+// indexes here are fucked, they start from 0...
+//  x 012345678901.
+// y
+// 0  ..............
+// 1  .......#XXX#..
+// 2  .......X...X..
+// 3  ..#XXXX#...X..
+// 4  ..X........X..
+// 5  ..#XXXXXX#.X..
+// 6  .........X.X..
+// 7  .........#X#..
+// 8  ..............
